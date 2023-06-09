2b5cef436e18e0e18b8a6a248f0c7d3d2b8711b1
da.malandin <da.malandin@tensor.ru>
2023-06-09 13:39:38 +0500

    1

--- homework10/task1.py
+++ homework10/task1.py
@@ -16,14 +16,21 @@
 import random
 import string
 
+
 def generate_random_name():
+
     letter = string.ascii_lowercase
     latin_words = []
-    for i in range(2):
-        words = ''.join([random.choice(letter) for i in range(random.randrange(1, 15, 1))])
-        latin_words.append(words)
-    return latin_words
-gen = generate_random_name()
-print(gen)
+    while True: # не знаем сколько раз позовут генератор
+        for i in range(2): #нам нужно 2 слова
+            words = ''.join([random.choice(letter) for x in range(random.randrange(1, 15, 1))]) #генерируем из набор букв слово в заданной длинне
+            latin_words.append(words) #апендим сгенерированное слово к пустому списку. генерим 2 слова и в цикле получим здесь 2 слова листом
+        yield " ".join(latin_words) #разбиваем список через пробел и делаем строкой, отдаем результат этого такта
+        latin_words = [] #чистим сгенерированные слова для новой итерации
 
-# Здесь пишем код
+
+gen = generate_random_name()
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
