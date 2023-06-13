002bb1755f4f4264fac708675b0cbbc96ddb0c95
da.malandin <da.malandin@tensor.ru>
2023-06-13 09:46:06 +0500

    1

--- homework10/task4.py
+++ homework10/task4.py
@@ -1,20 +1,22 @@
 # Создайте класс с тестами и напишите фикстуры в conftest.py:
 # 1) Фикстуру для класса и используйте её. Например, печать времени начала выполнения класса с тестами и окончания
 # 2) Фикстуру для конкретного теста и используйте её не для всех тестов. Например, время выполнения теста.
+import pytest
 
 
+@pytest.mark.usefixtures("start_end_time")
 class TestExample:
-    def test_1(self, start_end_time):
+    def test_1(self):
         pass
 
-    def test_2(self, start_end_time, execution_time):
+    def test_2(self, execution_time):
         pass
 
-    def test_3(self, start_end_time):
+    def test_3(self):
         pass
 
-    def test_4(self, start_end_time, execution_time):
+    def test_4(self, execution_time):
         pass
 
-    def test_5(self, start_end_time):
+    def test_5(self):
         pass
